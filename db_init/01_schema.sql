43a59487ee2faaaba379f90881f451524df2b5aa
yehuthi <yehuthith@gmail.com>
2025-06-17 22:36:21 +0300

    Initialize user and profile data

--- db_init/01_schema.sql
+++ db_init/01_schema.sql
@@ -1,17 +1,18 @@
-CREATE TABLE member(
-    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    email TEXT NOT NULL,
-    name TEXT NOT NULL,
-    password_hash TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+CREATE TABLE "user"(
+    "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "email" TEXT NOT NULL UNIQUE,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
-ALTER TABLE member OWNER TO "MYuser";
+CREATE UNIQUE INDEX user_email_lower_idx ON "user"(LOWER("email"));
+ALTER TABLE "user" OWNER TO "teder";
 
-CREATE UNIQUE INDEX member_email_lower_idx ON member(LOWER(email));
-
-CREATE TABLE session(
-    id TEXT PRIMARY KEY,
-    member INTEGER NOT NULL REFERENCES member(id) ON DELETE CASCADE,
-    expires_at TIMESTAMPTZ NOT NULL
+CREATE TABLE profile(
+    "user" INTEGER NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
+    "name" TEXT NOT NULL,
+    "gender" TEXT,
+    "birthday" JSON,
+    "bio" TEXT,
+    "avatar" TEXT
 );
-ALTER TABLE session OWNER TO "MYuser";
+ALTER TABLE profile OWNER TO "teder";
